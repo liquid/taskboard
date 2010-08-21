91f4ca816560f9a32d76beee5ff198c47146aaba
Attila Györffy <attila.gyorffy@gmail.com>
2010-08-21 11:56:11 +0100

    Upgrade the routes for Rails 3 compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Taskboard::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  
+  match 'home' => 'project#index', :as => 'home'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
@@ -49,10 +51,12 @@ Taskboard::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => 'project#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
