f8d46675084da0fc978e521738762918d2d11f57
Attila Györffy <attila.gyorffy@gmail.com>
2010-08-21 12:46:09 +0100

    Move code back to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  # just wrap and produce js like data structure
+  def burndown burndown_aware
+    data = burndown_aware.burndown.sort.to_a.map{ |x| 
+      # split string so we can get seconds from the epoch (as flot wants us to do)
+      [ make_time( x[0] ) * 1000, x[1] ]
+    }
+    data.inspect
+  end
+
+  def make_time(time_in_string)
+    time = time_in_string.split("-")
+    Time.utc(time[0], time[1], time[2], 0, 0, 0, 0).to_i
+  end
+  
 end
