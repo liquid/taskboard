c9bf973b1a5c6e4a8696add1f0081cc2a7876338
Attila Györffy <attila.gyorffy@gmail.com>
2010-09-12 19:30:36 +0200

    Fix deprecation warning (Base.after_destroy => after_destroy :method_name)

--- vendor/plugins/acts_as_taggable_on_steroids/lib/tagging.rb
+++ vendor/plugins/acts_as_taggable_on_steroids/lib/tagging.rb
@@ -2,7 +2,9 @@ class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :tag
   belongs_to :taggable, :polymorphic => true
   
-  def after_destroy
+  after_destroy :check_taggings
+  
+  def check_taggings
     if Tag.destroy_unused
       if tag.taggings.count.zero?
         tag.destroy
